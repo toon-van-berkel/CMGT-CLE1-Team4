59d11916da901d618bfc79dcc5fd94e44c7875c3
Toon-van-Berkel <toonvanberkel203@gmail.com>
2025-10-15 13:38:52 +0200

    comments

---
+++ CPX code/script.js
@@ -0,0 +1,46 @@
+<<<<<<< Updated upstream
+=======
+// loops.forever(function () {
+//     if (p === 100) {
+//         p = 0;
+//         OLED.drawLoading(p);
+//     } else {
+//         p++;
+//         OLED.drawLoading(p);
+//     }
+// })
+
+// OLED.init(128, 64);
+
+// let message = `Dit ${pins.A7.analogRead()}`;
+// // let message = "test";
+// OLED.writeStringNewLine(message);
+
+// loops.forever(function () {
+//     light.setPixelColor(0, Colors.Black);
+//     message = `Dit ${Math.floor(pins.A7.analogRead() / 100)}%`;
+//     OLED.writeStringNewLine(message);
+//     loops.pause(1000);
+//     light.setPixelColor(0, Colors.Red);
+//     // music.baDing.play()
+// })
+
+// light.showAnimation(light.rainbowAnimation, 500);
+
+// // crickit.motor1.run(100);
+// crickit.motor1.stop()
+
+// let message = "test";
+
+// loops.forever(function () {
+//     light.setPixelColor(0, Colors.Black);
+//     message = `TMEP ${input.temperature(TemperatureUnit.Celsius)}`;
+//     OLED.writeStringNewLine(message);
+//     loops.pause(1000);
+//     light.setPixelColor(0, Colors.Red);
+// })
+
+// OLED.drawCircle(10, 10, 10);
+// OLED.showString1("hello, micro:bit!")
+// OLED.showString2("Don't, micro:bit!")
+>>>>>>> Stashed changes
